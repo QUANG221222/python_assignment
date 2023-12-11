f3736b92a19d9d5d7a5489cff09236ed12d06fe3
QUANG221222 <nguyennhatquang.2509@gmail.com>
2023-12-11 13:31:20 +0700

    edit file

--- bai_hoc/bai_hoc_11_12.py
+++ bai_hoc/bai_hoc_11_12.py
@@ -1,3 +1,11 @@
+""" def Sum(a,b):
+    sum=0
+    for i in range(a,b+1):
+        sum+=i
+    return sum
+print("Sum of integers from 1 to 10 is",Sum(1,10))
+print("Sum of integers from 20 to 37 is",Sum(20,37))
+print("Sum of integers from 35 to 49 is",Sum(35,49)) """
 import math
 def isValid(side1, side2, side3):
     if (side1+side2)>side3 and (side1+side3)>side2 and (side2+side3)>side1:
@@ -13,5 +21,3 @@ def area(side1, side2, side3):
 
 a,b,c=eval(input("Enter 3 side of triangle:"))
 area(a,b,c)
-
-
